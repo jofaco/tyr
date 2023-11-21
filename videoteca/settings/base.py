e855021bdb6ab5179e65b3cbfc9dcd34df68bb76
marlong03 <36674053+jofaco@users.noreply.github.com>
2023-11-20 21:29:52 -0500

    Update base.py

--- videoteca/settings/base.py
+++ videoteca/settings/base.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://marlong03.github.io"
 
 ]
 
@@ -150,6 +151,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://marlong03.github.io"    
     
 ]
 
